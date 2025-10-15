96706e60a288bdfeec42c55c529a99325a1b4a3a
urantij <urantij@gmail.com>
2025-10-15 19:29:28 +0700

    Добавит бефор

--- Consts/Dota2DispenserParams.cs
+++ Consts/Dota2DispenserParams.cs
@@ -11,26 +11,36 @@ public class Dota2DispenserParams
     /// Айди матча в базе
     /// </summary>
     public const string dispenserMatchIdFilter = "dmatchid";
+
     /// <summary>
     /// Айди матча в доте
     /// </summary>
     public const string matchIdFilter = "matchid";
+
     /// <summary>
     /// 64
     /// </summary>
     public const string steamIdFilter = "steamid";
+
     /// <summary>
     /// Unix Seconds
     /// </summary>
     public const string afterDateTimeFilter = "after";
+
     /// <summary>
     /// Максимальное количество матчей. Не больше 20.
     /// </summary>
     public const string limitFilter = "limit";
+
     /// <summary>
     /// Включить только матчи, чей айди (не матч айди) больше либо равен параметру.
     /// </summary>
     public const string sinceIdFilter = "sinceid";
+
+    /// <summary>
+    /// Включить только матчи, чей айди (не матч айди) меньше параметра. 
+    /// </summary>
+    public const string beforeIdFilter = "beforeid";
 
     /// <summary>
     /// Имя хедера, куда класть свой айди
